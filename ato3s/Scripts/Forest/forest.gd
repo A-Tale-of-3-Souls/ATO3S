3081a4111e3968834c3244f23571c9c54d6e7afd
369px <168459560+369px@users.noreply.github.com>
2025-03-10 12:47:27 +0100

    2.5d camera rotation effect

--- ato3s/Scripts/Forest/forest.gd
+++ ato3s/Scripts/Forest/forest.gd
@@ -1,13 +1,50 @@
 extends Node2D
 
 
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
+var cam_velocity = Vector2.ZERO
+var cam_accelerat = 3
+var rotation_speed = 5 # Velocità di rotazione della telecamera
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+# Called every frame
 func _process(delta):
-	$Path2D/PathFollow2D.progress_ratio += 0.005
+	$Taco/Path2D/PathFollow2D.progress_ratio += 0.005
+	
+	var rotation_input = 0.0
+	
+	var rotation_movement = Vector2.ZERO
+	rotation_movement.x = Input.get_action_strength("cam_right") - Input.get_action_strength("cam_left")
+	rotation_movement.y = Input.get_action_strength("cam_up") - Input.get_action_strength("cam_down")
+	# Ruota la telecamera con i tasti freccia
+	if Input.is_action_pressed("cam_right"):
+		rotation_input += 1
+		cam_velocity = lerp(cam_velocity, 
+							rotation_movement * rotation_speed, 
+							cam_accelerat * delta
+						)
+		
+	if Input.is_action_pressed("cam_left"):
+		rotation_input -= 1
+		cam_velocity = lerp(cam_velocity, 
+							rotation_movement * rotation_speed, 
+							cam_accelerat * delta
+						)
 	
 	
+	# Cam rotation effect
+	# 1. Rotate the whole map
+	rotation += rotation_input * rotation_speed * delta
+	# 2. Rotate Taco Stack of sprites (all at the same time) could be done in a loop
+	get_child(0).get_child(0).rotation += (-rotation_input*rotation_speed*delta)
+	# 3. Rotate each taco sprite for 3d effect
+	for child in get_child(0).get_child(0).get_children():
+		child.rotation += (rotation_input*rotation_speed*delta)
+	# 4. Rotate Tree Stack of sprites (all at the same time)
+	get_child(1).get_child(0).rotation += (-rotation_input*rotation_speed*delta)
+	# 5. Rotate each tree sprite for 3d effect
+	for child in get_child(1).get_child(0).get_children():
+		child.rotation += (rotation_input*rotation_speed*delta)
--- ato3s/scenes/taco.gd
+++ ato3s/scenes/taco.gd
@@ -2,14 +2,23 @@ extends CharacterBody2D
 
 #var velocity = Vector2.ZERO #GODOT ERROR SAYS: Member "velocity" redefined (original in nativa class CharacterBody2D)
 var SPEED = 139
-var ACCELERAT = 9
+var ACCELERAT = 20
 
-func _physics_process(delta):
-	var input = Vector2.ZERO
+func _physics_process(delta): # runs each frame
+	var input = get_user_input()
+	update_velocity(input, delta)
+	move_and_slide()
+	rotate_stack(input)
+
+func get_user_input():
+	var input = Vector2.ZERO # initialize input on each frame
 	input.x = Input.get_action_strength("Right") - Input.get_action_strength("Left")
 	input.y = Input.get_action_strength("Down") - Input.get_action_strength("Up")
+	return input
+
+func update_velocity(input, delta):
 	velocity = lerp(velocity, input * SPEED, ACCELERAT * delta)
-	move_and_slide()
-	
-	if input != Vector2.ZERO:
-		$SpriteStack._set_rotation(velocity.angle())
+
+func rotate_stack(input):
+	if input != Vector2.ZERO: # if user is moving
+		$SpriteStack._set_rotation(velocity.angle()) # rotate stack based on velocity angle
