02f3465d020b051c0b02ed29fbe4bab53d984f7b
369px <168459560+369px@users.noreply.github.com>
2025-04-19 15:23:49 +0200

    ROTATION SMOOOOOOOOTH
    SO SMOOTH. SO COOL. (yves and 369px)

--- ato3s/Scripts/Forest/tree.gd
+++ ato3s/Scripts/Forest/tree.gd
@@ -7,14 +7,3 @@ const JUMP_VELOCITY = -400.0
 
 func _physics_process(delta):
 	$SpriteStack.get_child(0).z_index = -1
-
-
-func _on_area_2d_body_entered(body):
-	if body is Taco:
-		z_index = 1
-
-
-
-func _on_area_2d_body_exited(body):
-	if body is Taco:
-		z_index = 0
--- ato3s/scenes/camera_2d.gd
+++ ato3s/scenes/camera_2d.gd
@@ -2,118 +2,56 @@ extends Camera2D
 # camera variables
 var cam_velocity = Vector2.ZERO
 var cam_accelerat = 1
-var rotation_speed = 5 # Velocità di rotazione della telecamera
-@onready var player = get_parent()
-@onready var map_entities = $"../../MapEntities"
-#@onready var fix_collision = $"../../Fix_Collision_shape"
+var rotation_speed = 10  # Velocità di rotazione desiderata (radians/second)
+var current_rotation_speed = 0.0  # Velocità attuale interpolata
+var rotation_smoothness = 10.0  # Fattore di smoothing
 
+@onready var player = get_parent().get_parent()
+@onready var map_entities = $"../../../MapEntities"
+@onready var generators = $"../../../Generators"
 
-# Called every frame
-func _process(delta):
-	position = GameManager.taco.position
-	
+# Usiamo _physics_process per un movimento più fluido
+func _physics_process(delta):
+	handle_camera_rotation(delta)
+
+func handle_camera_rotation(delta):
 	var rotation_input = 0.0
 	
-	var rotation_movement = Vector2.ZERO
-	rotation_movement.x = Input.get_action_strength("cam_right") - Input.get_action_strength("cam_left")
-	rotation_movement.y = Input.get_action_strength("cam_up") - Input.get_action_strength("cam_down")
-	#if rotation_movement != Vector2.ZERO:
-		#GameManager.camera_rotating = true
-		# Ruota la telecamera con i tasti freccia
+	# Calcola l'input
 	if Input.is_action_pressed("cam_right"):
 		rotation_input += 1
-		#cam_velocity = lerp(cam_velocity, 
-							#rotation_movement * rotation_speed, 
-							#cam_accelerat * delta
-						#)
-		rotate_camera(rotation_input, delta)
-		
 	if Input.is_action_pressed("cam_left"):
 		rotation_input -= 1
-		#cam_velocity = lerp(cam_velocity, 
-							#rotation_movement * rotation_speed, 
-							#cam_accelerat * delta
-						#)
-		rotate_camera(rotation_input, delta)
-		#else:
-			#ameManager.camera_rotating = false
-
-		
 	
+	# Interpola la velocità di rotazione
+	var target_speed = rotation_input * rotation_speed
+	current_rotation_speed = lerp(current_rotation_speed, target_speed, delta * rotation_smoothness)
 	
-	
-			
-func rotate_camera(rotation_input, delta):
-	#$Taco/steps_on_snow.emitting = false
-	# Cam rotation effect
-	# 1. Rotate the whole map
-	
-	var rotation_value = (rotation_input*rotation_speed*delta)
-	var fix_rotation = -rotation_value
-	
-	rotate_world(rotation_value, fix_rotation)
-	rotate_player(rotation_value,fix_rotation)
-	
-
-
-
+	# Applica la rotazione solo se c'è input
+	if abs(current_rotation_speed) > 0.01:
+		var rotation_value = current_rotation_speed * delta
+		var fix_rotation = -rotation_value
+		rotate_world(rotation_value, fix_rotation)
+		rotate_player(rotation_value, fix_rotation)
 
 func rotate_sprite_stacks(stack, rotation_val):
-	for sprite in stack.get_children():#for every sprite in stack
-		sprite.rotation += rotation_val#rotate it with map entities
-		
-
+	for sprite in stack.get_children():
+		# Usiamo la rotazione locale invece di global_rotation
+		sprite.rotation += rotation_val
 
 func rotate_player(rotation_value, fix_rotation):
-	player.rotation += rotation_value
-	#player.get_child(0)
-	
+	player.global_rotation += rotation_value
 	var taco_object = player.get_child(0)
-	var taco_stack = taco_object.get_child(0)#get spritestack of map object
-	taco_stack.rotation += fix_rotation # fix rotate stack of sprites opposite of map entities
+	var taco_stack = taco_object.get_child(0)
 	
+	# Correzione locale invece che globale
+	taco_stack.rotation += fix_rotation
 	rotate_sprite_stacks(taco_stack, rotation_value)
-	
-	var collision_shape = taco_object.get_child(1)
-	#collision_shape.rotation+=fix_rotation
-	
-	#if GameManager.player_moving:
-		##if GameManager.player_input != Vector2.ZERO:
-		#collision_shape.rotation+=fix_rotation
-		##collision_shape.rotation = taco_stack.rotation
-	#else:
-		#collision_shape.rotation = taco_stack.get_child(0).rotation
-
 
 func rotate_world(rotation_value, fix_rotation):
 	map_entities.rotation += rotation_value
-	#GameManager.taco.rotation += fix_rotation
-	#GameManager.taco.collision.rotation = GameManager.taco.velocity.angle()
-	for child in map_entities.get_children(): # for every child of map_entities
-		if child.get_children():#if map object
-			var stack = child.get_child(0)#get spritestack of map object
-			stack.rotation += fix_rotation # rotate stack of sprites opposite of map entities
+	for child in map_entities.get_children():
+		if child.get_children():
+			var stack = child.get_child(0)
+			stack.rotation += fix_rotation
 			rotate_sprite_stacks(stack, rotation_value)
-			#var collision_shape = child.get_child(1)#get collisionshape of entity
-			#if GameManager.player_moving and child is Taco:
-				#pass
-			#else:
-				#collision_shape.rotation += fix_rotation
-		#if child.get_children(): # if it has children
-			#var stack = child.get_child(0)
-			#var area = child.get_child(2)
-			#var collision_child = child.get_child(1)
-			#stack.rotation += (-rotation_input*rotation_speed*delta) # rotate stack of sprites
-			## 3. Rotate each taco sprite for 3d effect
-			#if collision_child is CollisionShape2D:
-				##if GameManager.player_moving:
-				##GameManager.taco.collision.rotation = GameManager.taco.velocity.angle()
-				#collision_child.rotation += (-rotation_input*rotation_speed*delta)
-			##if area is Area2D:
-				##area.rotation += (-rotation_input*rotation_speed*delta)
-			#for nephew in stack.get_children(): 
-				#nephew.rotation += (rotation_input*rotation_speed*delta)
-			## 4. Rotate Tree Stack of sprites (all at the same time)
-		#else: # particles or eother stuff we don't wanna move
-			#child.rotation += (-rotation_input*rotation_speed*delta)
-			
